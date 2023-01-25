34f4f42b4ce78f2b8912341590fa0c3bb93f2494
Mayke Erick <maykeesa.contato@gmail.com>
2023-01-25 09:29:03 -0300

    fix: Repassando para H2

--- src/main/java/br/com/attornatus/AvaliacaoAttornatusApplication.java
+++ src/main/java/br/com/attornatus/AvaliacaoAttornatusApplication.java
@@ -1,12 +1,31 @@
 package br.com.attornatus;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.attornatus.model.Pessoa;
+import br.com.attornatus.repository.PessoaRepository;
+
 @SpringBootApplication
-public class AvaliacaoAttornatusApplication {
+public class AvaliacaoAttornatusApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvaliacaoAttornatusApplication.class, args);
+	}
+	
+	@Autowired
+	private PessoaRepository pessoaRep;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		//Pessoas
+		Pessoa p1 = new Pessoa("Mayke", "16/03/2003");
+		Pessoa p2 = new Pessoa("Bruno", "19/10/2002");
+		Pessoa p3 = new Pessoa("Soares", "22/12/2002");
+		this.pessoaRep.save(p1);
+		this.pessoaRep.save(p2);
+		this.pessoaRep.save(p3);
 	}
 }
--- src/main/java/br/com/attornatus/repository/ResidenciasRepository.java
+++ src/main/java/br/com/attornatus/repository/ResidenciasRepository.java
@@ -12,10 +12,10 @@ import br.com.attornatus.model.Residencia;
 public interface ResidenciasRepository extends JpaRepository<Residencia, Long>{
 
 	@Query(nativeQuery = true, value = "SELECT * FROM "
-						 + "residencias pe WHERE pe.id_endereco = :idEndereco")
+						 + "residencias re WHERE re.id_endereco = :idEndereco")
 	public List<Residencia> findIdEndereco(Long idEndereco);
 	
 	@Query(nativeQuery = true, value = "SELECT * FROM "
-			 + "residencias pe WHERE pe.id_pessoa = :idPessoa")
+			 + "residencias re WHERE re.id_pessoa = :idPessoa")
 	public List<Residencia> findIdPessoa(Long idPessoa);
 }
